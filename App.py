fe9123b0f4ef748755f2f5299dc9a7085e3137f9
Mahiler <fernando.mahiler@gmail.com>
2020-05-16 04:40:39 -0500

    segudo commit

--- App.py
+++ App.py
@@ -1,30 +1,41 @@
 from flask import Flask, render_template, request, redirect , url_for
 from flask_mysqldb import MySQL
 
-
 app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_PASSWORD'] = 'Lerietbool999'
 app.config['MYSQL_DB'] = 'farmacia_inventario'
-
 mysql = MySQL(app)
-
-# con = MySQLdb.connect(host="", user="", password="", db="");
-
 
 @app.route("/")
 def Index():
     return render_template("index.html", title="sign Up")
 
-
 @app.route("/checkUser", methods=["POST"])
 def check():
     username = str(request.form["user"])
     password = str(request.form["password"])
-    cursor = conn.cursor()
-    cursor.execute("")
-    user = cursor.fetchone()
+    cursor = mysql.connection.cursor()
+    cursor.execute("select * from usuario where nombreUsuario = %s and contraseña = %s  ", (username, password))
+    data = cursor.fetchall()
+    if len(data) == 1:
+        return redirect(url_for("home"))
+    else:
+        return "Failed"
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route("/users")
+def users():
+    return render_template("usuarios.html")
+
+
+@app.route("/inventory")
+def inventory():
+    return render_template("inventario.html")
 
 
 
